f87449631e21a4156f1ed7c7e516f2b798155287
Lukas Kollmer <lukas.kollmer@me.com>
2014-11-23 08:39:56 +0100

    Initial Commit

---
+++ FreeSpace/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FreeSpace
+//
+//  Created by Lukas Kollmer on 23/11/14.
+//  Copyright (c) 2014 Lukas Kollmer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
